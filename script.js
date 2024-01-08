6e5279d9abe28a32480d3a9455e7ac0494bf1787
CodzSword <124258380+codzsword@users.noreply.github.com>
2024-01-08 14:22:34 +0545

    Initial Commit

---
+++ script.js
@@ -0,0 +1,19 @@
+// Tab Interface
+
+const resumeHeading = document.querySelector(".tab-heading");
+const resumeTabs = document.querySelectorAll(".tab");
+
+resumeHeading.onclick = (event) => {
+  event.preventDefault();
+  const clickedItemId = event.target.dataset.id;
+  if (clickedItemId) {
+    resumeHeading.querySelector(".active").classList.remove("active");
+    event.target.classList.add("active");
+
+    resumeTabs.forEach((tab) => {
+      tab.classList.remove("active");
+    });
+    const correspondingTab = document.getElementById(clickedItemId);
+    correspondingTab.classList.add("active");
+  }
+};
